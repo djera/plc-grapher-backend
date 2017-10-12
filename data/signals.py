686b2f6d0dd85374fc3e0dad3645923616e17916
DESKTOP-6FV0VEM\nejc <nejc.robnik@gmail.com>
2017-10-12 09:33:12 +0200

    working signals

--- data/signals.py
+++ data/signals.py
@@ -8,7 +8,6 @@ from chans.wssender import graph_update
 @receiver(post_save, sender=HexData)
 def send_graph_data(sender, instance=None, created=False, **kwargs):
     if instance is not None:
-
         config = DataModelConfig.objects.filter(user=instance.user).first()
         variables = VariableConfig.objects.filter(config=config)
 
@@ -19,9 +18,8 @@ def send_graph_data(sender, instance=None, created=False, **kwargs):
         for v in variables:
             if v.type == VariableConfig.TYPES[0][0]:
                 if v.startByte < b_len:
-                    print(str(b[v.startByte]) + ' % '  + str(pow(2, v.startBit)))
                     value = b[v.startByte] & pow(2, v.startBit)
                     data[v.name] = value
 
-        data["timestamp"] = instance.added_at
+        data["timestamp"] = instance.added_at.strftime("%Y-%m-%d %H:%M:%S")
         graph_update(data)
